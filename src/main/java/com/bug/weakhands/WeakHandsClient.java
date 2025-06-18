12e9d93fc89a1aebf90248374c8125b208592136
bug <gabehanby03@gmail.com>
2025-06-17 23:40:39 -0500

    Initial commit

---
+++ src/main/java/com/bug/weakhands/WeakHandsClient.java
@@ -0,0 +1,7 @@
+package com.bug.weakhands;
+
+import net.fabricmc.api.ClientModInitializer;
+public class WeakHandsClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {}
+}
